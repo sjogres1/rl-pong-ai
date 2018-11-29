5e7b703098cdff24b02413ebbbc8dabf916410ee
sjogres1 <sauli.sjogren@aalto.fi>
2018-11-29 19:22:15 +0200

    saulin koneen paskat

--- agent.py
+++ agent.py
@@ -1,6 +1,8 @@
 """ Our Agent that needs to beat the SImpleAI """
 from pong import Pong
-# from policy import Policy
+from policy import Policy
+
+
 
 class Agent(object):
     def __init__(self, env, player_id=1):
@@ -8,15 +10,24 @@ class Agent(object):
         self.player_id = player_id
         self.bpe = 4
         self.name = "uber_AI"
-        self.actions = [self.env.MOVE_UP, self.env.MOVE_DOWN, self.env.STAY]
-        self.actionspace = 3
+        self.train_device = "cpu"
+       # self.policy = policy.to(self.train_device)
+        #self.optimizer = torch.optim.RMSprop(policy.parameters(),lr=5e-3)
+        self.batch_size = 3
+        self.gamma = 0.99
+        self.observations = []
+        self.actions = []
+        self.rewards = []
+
+
 
     def get_name(self):
         return self.name
 
     def get_action(self, ob=None):
-        player = self.env.player1
+        player = self.env.player1 if self.player_id == 1 else self.env.player2
         action = self.env.MOVE_UP
+
 
         return action
 
@@ -25,36 +36,4 @@ class Agent(object):
         return
 
 
-""" Links that could be possible solutions to solve this problem:
 
-https://medium.com/@dhruvp/how-to-write-a-neural-network-to-play-pong-from-scratch-956b57d4f6e0
-
-http://www.cs.huji.ac.il/~ai/projects/2014/Pong/files/report.pdf
-
-https://codeincomplete.com/posts/javascript-pong/part5/
-
-https://github.com/topics/pong-game
-
-http://nifty.stanford.edu/2018/guerzhoy-pong-ai-tournament/
-
-https://www.quora.com/How-do-I-improve-my-paddle-pong-AI
-
-http://www.pygame.org/project-py-pong-2040-.html
-
-https://medium.com/mlreview/speeding-up-dqn-on-pytorch-solving-pong-in-30-minutes-81a1bd2dff55
-
-https://arxiv.org/abs/1807.08452
-
-https://becominghuman.ai/lets-build-an-atari-ai-part-1-dqn-df57e8ff3b26
-
-https://www.youtube.com/watch?v=g9svjZxpug0
-
-https://www.youtube.com/watch?v=YFe0YbaRIi8
-
-https://www.reddit.com/r/MachineLearning/comments/3y16pa/i_trained_a_deep_q_network_built_in_tensorflow_to/
-
-https://www.learndatasci.com/tutorials/reinforcement-q-learning-scratch-python-openai-gym/
-
-https://ray.readthedocs.io/en/latest/example-rl-pong.html
-
-"""
--- policy.py
+++ policy.py
@@ -1,25 +1,37 @@
 import torch
 import torch.nn.functional as F
-from utils import discount_rewards, softmax_sample
+#from utils import discount_rewards, softmax_sample
+
+def preprocess(image):
+    # Remove colors
+    image = image[:,:,0]
+    # Downsample
+    image = image[::2,::2,:]
+    # Remove non gaming area
+
 
 
 class Policy(torch.nn.Module):
-    def __init__(self, state_space, action_space):
+    def __init__(self, action_space):
+        # Create neural network
         super().__init__()
-        self.state_space = state_space
-        self.action_space = action_space
-        self.fc1 = torch.nn.Linear(state_space, 12)
-        self.fc2 = torch.nn.Linear(12, action_space)
+        self.conv_1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
+        self.conv_2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+        self.conv_3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.lin1 = torch.nn.Linear(3136, 784)
+        self.lin2 = torch.nn.Linear(784, action_space)
         self.init_weights()
 
-    def init_weights(self):
-        for m in self.modules():
-            if type(m) is torch.nn.Linear:
-                torch.nn.init.uniform_(m.weight)
-                torch.nn.init.zeros_(m.bias)
+    # def init_weights(self):
+    #     for m in self.modules():
+    #         if type(m) is torch.nn.Linear:
+    #             torch.nn.init.uniform_(m.weight)
+    #             torch.nn.init.zeros_(m.bias)
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = F.relu(x)
-        x = self.fc2(x)
+        x = F.relu(self.conv_1(x))
+        x = F.relu(self.conv_2(x))
+        x = F.relu(self.conv_3(x))
+        x = F.relu(self.lin1(x.view(x.size(0), -1)))
+        x = self.lin2(x)
         return F.softmax(x, dim=-1)

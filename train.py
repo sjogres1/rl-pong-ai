c38ac78595435873325488759e79f01dab672756
Jan Gustafsson <jan.gustafsson@fi.abb.com>
2018-11-30 15:10:42 +0200

    Added saving model after crtl+c

--- train.py
+++ train.py
@@ -42,41 +42,45 @@ player = Agent(env, player_id)
 
 env.set_names(player.get_name(), opponent.get_name())
 
-for episode_num in range(0, episodes):
-    reward_sum, timesteps = 0, 0
-    done = False
-    # Reset the environment and observe initial states for both players
-    (ob1, ob2) = env.reset()
-    while not done:
+try:
+    for episode_num in range(0, episodes):
+        reward_sum, timesteps = 0, 0
+        done = False
+        # Reset the environment and observe initial states for both players
+        (ob1, ob2) = env.reset()
+        while not done:
 
-        # Get actions for both agents
-        action1, aprob = player.get_action(ob1, episode_num)
-        action2 = opponent.get_action()
-        # Save previous observation for our player
-        prev_ob1 = ob1
+            # Get actions for both agents
+            action1, aprob = player.get_action(ob1, episode_num)
+            action2 = opponent.get_action()
+            # Save previous observation for our player
+            prev_ob1 = ob1
 
-        # Make an action with both players
-        (ob1, ob2), (rew1, rew2), done, info = env.step((action1, action2))
-       
-
-        # Store action's outcome (so that the agent can improve its policy)
-        player.store_outcome(prev_ob1, aprob, action1, rew1)
+            # Make an action with both players
+            (ob1, ob2), (rew1, rew2), done, info = env.step((action1, action2))
         
-        # Store total episode reward
-        timesteps += 1
-        reward_sum += rew1
 
-        if not args.headless:
-            env.render()
-        if done:
-            # ob1.tofile('observation.txt', ';')
-            #observation = env.reset()
-            #plot(ob1) # plot the reset observation
-            print("episode {} over, reward: {} \t({} timesteps)".format(episode_num, reward_sum, timesteps))
+            # Store action's outcome (so that the agent can improve its policy)
+            player.store_outcome(prev_ob1, aprob, action1, rew1)
+            
+            # Store total episode reward
+            timesteps += 1
+            reward_sum += rew1
 
-    player.episode_finished(episode_num)
+            if not args.headless:
+                env.render()
+            if done:
+                # ob1.tofile('observation.txt', ';')
+                #observation = env.reset()
+                #plot(ob1) # plot the reset observation
+                print("episode {} over, reward: {} \t({} timesteps)".format(episode_num, reward_sum, timesteps))
 
-save_model_final(player)
+        player.episode_finished(episode_num)
+    save_model_final(player)
+    
+except KeyboardInterrupt:
+    print("Interrupted!")
+    save_model_final(player)
 
 # Needs to be called in the end to shut down pygame
 env.end()

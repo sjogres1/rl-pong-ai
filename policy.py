9db3847c6f9ecad7364e8693cb1d93dc13a50bd3
sjogres1 <sauli.sjogren@aalto.fi>
2018-12-04 17:54:55 +0200

    normalized neural network data before feeding it to softmax function

--- policy.py
+++ policy.py
@@ -43,9 +43,9 @@ class Policy(torch.nn.Module):
         # Softmax returns a probality of each action.
 
         # If still does not learn, we can try to normalize the x values before feeding them to softmax
-        # x_max = torch.max(x)
-        x = x - torch.mean(x)
-        # x = torch.div(x,x_max)
+         x_max = torch.max(x)
+        #x = x - torch.mean(x)
+         x = torch.div(x,x_max)
         
         return F.softmax(x, dim=-1) # should this be 1?
 
